9b3a5052fe52b750eb746cf013daf90f2c5c4f95
Md. Masud Rana <masudrana.mwp@gmail.com>
2026-01-05 17:22:00 +0600

    Added more object

--- src/Jan05/Main.java
+++ src/Jan05/Main.java
@@ -11,10 +11,17 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Person p = new Person(1292522, "Masud", 30000);
+        Person p1 = new Person(1292593, "Woalinur", 30000);
+        Person p2 = new Person(1292599, "Akib", 30000);
+        Person p3 = new Person(1292520, "Shohel", 30000);
+        
         
         List list = new ArrayList();
         
         list.add(p);
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
         
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
